2044fd5a0f67f8b852a16745405d13bedc3812c8
lilong <lilong@abluepoint.com>
2023-12-25 14:40:13 +0800

    demo

--- src/pages/editor/milkdown/index.tsx
+++ src/pages/editor/milkdown/index.tsx
@@ -29,13 +29,41 @@ export default function MilkDownEditorPage() {
     if (lock) return;
     console.log('markdown', markdown);
   }, []);
+
+  const content = `# Milkdown
+  ![greeting bear](/polar.jpeg)
+
+> Milkdown is a WYSIWYG markdown editor framework.
+>
+> 🍼 Here is the [repo](https://github.com/Milkdown/milkdown) (right click to open link). \
+> We ~~only support commonmark~~. GFM is also supported!
+
+You can check the output markdown text in **two columns editing**.
+
+* Features
+  * [x] 📝 **WYSIWYG Markdown** - Write markdown in an elegant way
+  * [x] 🎨 **Themable** - Theme can be shared and used with npm packages
+  * [x] 🎮 **Hackable** - Support your awesome idea by plugin
+  * [x] 🦾 **Reliable** - Built on top of [prosemirror](https://prosemirror.net/) and [remark](https://github.com/remarkjs/remark)
+  * [x] ⚡ **Slash & Tooltip** - Write fast for everyone, driven by plugin
+  * [x] 🧮 **Math** - LaTeX math equations support, driven by plugin
+  * [x] 📊 **Table** - Table support with fluent ui, driven by plugin
+  * [x] 📰 **Diagram** - Diagram support with [mermaid](https://mermaid-js.github.io/mermaid/#/), driven by plugin
+  * [x] 🍻 **Collaborate** - Shared editing support with [yjs](https://docs.yjs.dev/), driven by plugin
+  * [x] 💾 **Clipboard** - Support copy and paste markdown, driven by plugin
+  * [x] 👍 **Emoji** - Support emoji shortcut and picker, driven by plugin
+* Made by
+  * Programmer: [Mirone](https://github.com/Milkdown)
+  * Designer: [Mirone](https://github.com/Milkdown)
+`;
+
   return (
     <Card>
       <Title heading={6}>{'Milkdown Editor'}</Title>
       <Provider>
         <div>
           <PlaygroundMilkdown
-            content="你好"
+            content={content}
             milkdownRef={milkdownRef}
             onChange={onMilkdownChange}
           ></PlaygroundMilkdown>
